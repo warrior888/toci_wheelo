4ff41f52eed530c42ec25ef9869a3c98cd1f6162
Bartomiej Zapart <bzapart@hotmail.com>
2020-03-12 08:17:00 +0100

    web api json scaffold

---
+++ Toci.Driver.Bll.Porsche.Interfaces/Toci.Driver.Ui.Api/Areas/Registration/RegistrationAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Toci.Driver.Ui.Api.Areas.Registration
+{
+    public class RegistrationAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Registration";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Registration_default",
+                "Registration/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Toci.Driver.Bll.Porsche.Interfaces/Toci.Driver.Ui.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Toci.Driver.Ui.Api.WebApiApplication" Language="C#" %>
