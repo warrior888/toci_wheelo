08177f73914bb1a3e97b88e0b1952eb86481125e
Adrian Zach <adrien997@gmail.com>
2021-03-07 17:04:58 +0100

    creating cars from JSON :-)

--- Wheelo/Wheelo.Tests/Wheelo.Test/Cars/DatabaseOperability.cs
+++ Wheelo/Wheelo.Tests/Wheelo.Test/Cars/DatabaseOperability.cs
@@ -3,30 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wheelo.Nfs.Database.Persistence;
 using Wheelo.Nfs.Database.Persistence.Models;
 
 namespace Wheelo.Test.Cars
 {
-    class DatabaseOperability
+    public class DatabaseOperability
     {
 
+        
 
-
-        protected void createCarEntity(CarEntity carek)
+        public int Reptile(Dictionary<string, List<CarEntity>> skrillex)
         {
+
+            PostgresqlRepository<Carsbrand> repouser = new PostgresqlRepository<Carsbrand>();
+
+            foreach (KeyValuePair<string, List<CarEntity>> item in skrillex)
+            {
+                Carsbrand ent = createCarEntity(item.Value.First());
+
+                repouser.Insert(ent);
+            }
+
+
+            /* [ foreach (  )
+                    { BMW,  List< CarEntity, CarEntity, CarEntity ... > }, 
+                    { Seat,  List< CarEntity, CarEntity, CarEntity ... > } 
+               ]
+            */
+
+            return 1;
+        }
+
+        protected Carsbrand createCarEntity(CarEntity carek)
+        {
+
+           
 
             Carsbrand c = new Carsbrand();
 
             c.Brand = carek.make;
 
+            
 
 
+            return c;
             // {"year": 2016, "id": 1, "horsepower": 201, "make": "acura", "model": "ilx", "price": 31890.0, "img_url": "http://ts2.mm.bing.net/th?id=OIP.M7656c6f6aaa4d51c3ddee4b1192adeeeH0&pid=15.1"}
-
-
-
-
-
+             
             /*
                 public Carsbrand()
                 {
