778cfe7d1e051df035f06da5d078d6702c95b2f0
Tomek <tomek1989gw@wppl>
2021-03-07 13:56:28 +0100

    Brutality update!

---
+++ Wheelo/Wheelo.Tests/Wheelo.Test/Cars/CarEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wheelo.Test.Cars
+{
+    public class CarEntity
+    {
+        public int year { get; set; }
+        public int id { get; set; }
+        public double horsepower { get; set; }
+        public string make { get; set; }
+        public string model { get; set; }
+        public double price { get; set; }
+        public string img_url { get; set; }
+    }
+}
---
+++ Wheelo/Wheelo.Tests/Wheelo.Test/Genetaror/Subzero.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wheelo.Nfs.Database.Persistence;
+using Wheelo.Nfs.Database.Persistence.Models;
+using System.Net.Http;
+
+namespace Wheelo.Test.Generator
+{
+    [TestClass]
+    public class SubZero
+    {
+        protected JohnnyCage FinishHim = new JohnnyCage();
+
+        [TestMethod]
+        public async Task BrutalityAsync()
+        {
+            var baseAddress = new Uri("https://private-anon-a2c354519e-carsapi1.apiary-mock.com/");
+
+            using (var httpClient = new HttpClient { BaseAddress = baseAddress })
+            {
+
+                using (var response = await httpClient.GetAsync("cars"))
+                {
+
+                    string responseData = await response.Content.ReadAsStringAsync();
+
+                    responseData = 
+
+
+
+
+
+                }
+            }
+
+
+
+
+            Fatality(50);
+
+        }
+
+        public int Fatality(int numRows)
+        {
+
+            for (int i = 0; i < numRows; i++)
+            {
+                string Email = FinishHim.GetRandomWord(8);
+                string Login = FinishHim.GetRandomWord(5);
+                string Name = FinishHim.GetRandomWord(5);
+                string Passoword = FinishHim.GetRandomWord(5);
+                string Surname = FinishHim.GetRandomWord(5);
+                string phone = FinishHim.GetRandomWord(5);
+
+
+                User x = new User()
+                {
+                    Email = Email + i,
+                    Login = Login + i,
+                    Name = Name + i,
+                    Password = Passoword + i,
+                    Surname = Surname + i,
+                    Phone = phone + i,
+
+                };
+
+
+
+                PostgresqlRepository<User> repouser = new PostgresqlRepository<User>();
+                repouser.Insert(x);
+            }
+
+
+            return 1;
+
+
+
+        }
+    }
+}
